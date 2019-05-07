1db966732b41f9f6532b26651ad7e208f7f963ba
eunyeoul <eunyull102@nate.com>
2019-05-07 22:54:59 +0900

    [update] gradle 수정 및 토큰 값 가져오기 로직 추가

--- app/src/main/java/com/test/mysamplepush/FireBaseMessagingService.kt
+++ app/src/main/java/com/test/mysamplepush/FireBaseMessagingService.kt
@@ -1,123 +1,76 @@
 package com.test.mysamplepush
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.support.v4.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class FireBaseMessagingService : FirebaseMessagingService() {
 
+    /**
+     * 구글 토큰을 얻는 값입니다.
+     * 아래 토큰은 앱이 설치된 디바이스에 대한 고유값으로 푸시를 보낼때 사용됩니다.
+     * **/
+    override fun onNewToken(s: String?) {
+        super.onNewToken(s)
+    }
+
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
 
         if (remoteMessage?.data?.size!! > 0) {
+            sendNotification(remoteMessage)
+        }
 
+    }
+
+    private fun sendNotification(remoteMessage: RemoteMessage?) {
+
+        val title = remoteMessage?.data?.get("title")
+        val message = remoteMessage?.data?.get("message")
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val channel = "채널"
+            val channel_nm = "채널명"
+
+            val notiChannel = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val channelMessage = NotificationChannel(channel, channel_nm, NotificationManager.IMPORTANCE_DEFAULT)
+            channelMessage.description = "채널에 대한 설명"
+            channelMessage.enableLights(true)
+            channelMessage.enableVibration(true)
+            channelMessage.setShowBadge(false)
+            channelMessage.vibrationPattern = longArrayOf(100, 200, 100, 200)
+            notiChannel.createNotificationChannel(channelMessage)
+
+            val notificationBuilder = NotificationCompat.Builder(this, channel)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setChannelId(channel)
+                .setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(9999, notificationBuilder.build())
+
+        } else {
+            val notificationBuilder = NotificationCompat.Builder(this,"")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(title)
+                .setContentTitle(message)
+                .setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(9999, notificationBuilder.build())
 
         }
 
-        sendNotification(remoteMessage?.notification?.body)
-
     }
-
-    private fun sendNotification(messageBody: String?) {
-
-
-    }
-
-
-//    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
-//
-//        Log.d(TAG, "From: " + remoteMessage!!.from!!)
-//
-//
-//
-//        if (remoteMessage.data.size > 0) {
-//
-//            Log.d(TAG, "Message data payload: " + remoteMessage.data)
-//
-//
-//
-//            if (true) {
-//
-//            } else {
-//
-//                handleNow()
-//
-//            }
-//
-//        }
-//
-//        if (remoteMessage.notification != null) {
-//
-//            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body!!)
-//
-//            sendNotification(remoteMessage.notification!!.body)
-//
-//        }
-//
-//    }
-//
-//    private fun handleNow() {
-//
-//        Log.d(TAG, "Short lived task is done.")
-//
-//    }
-//
-//
-//    private fun sendNotification(messageBody: String?) {
-//
-//        val intent = Intent(this, MainActivity::class.java)
-//
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-//
-//        val pendingIntent = PendingIntent.getActivity(
-//            this, 0, intent,
-//
-//            PendingIntent.FLAG_ONE_SHOT
-//        )
-//
-//
-//        //        String channelId = getString(R.string.default_notification_channel_id);
-//
-//        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-//
-//        val notificationBuilder =
-//
-//            NotificationCompat.Builder(this, channelId)
-//
-//                .setSmallIcon(R.mipmap.ic_launcher)
-//
-//                .setContentTitle("FCM Message")
-//
-//                .setContentText(messageBody)
-//
-//                .setAutoCancel(true)
-//
-//                .setSound(defaultSoundUri)
-//
-//                .setContentIntent(pendingIntent)
-//
-//
-//        val notificationManager =
-//
-//            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//
-//
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//
-//            val channelName = getString(R.string.default_notification_channel_name)
-//
-//            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
-//
-//            notificationManager.createNotificationChannel(channel)
-//
-//        }
-//
-//        notificationManager.notify(0, notificationBuilder.build())
-//
-//    }
-//
-//    companion object {
-//
-//        private val TAG = "MyFirebaseMsgService"
-//    }
 
 }
--- app/src/main/java/com/test/mysamplepush/MainActivity.kt
+++ app/src/main/java/com/test/mysamplepush/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.test.mysamplepush
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener { instanceIdResult ->
+            val newToken = instanceIdResult.token
+            Log.e("onCreate",newToken)
+        }
     }
 }
