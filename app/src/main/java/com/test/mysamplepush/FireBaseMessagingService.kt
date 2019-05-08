3b699395d2fdcc7c3efc2f794d66fb8d039d7bc3
eunyeoul <eunyull102@nate.com>
2019-05-08 12:13:57 +0900

    [update] fcm 테스트 이미지 포함

--- app/src/main/java/com/test/mysamplepush/FireBaseMessagingService.kt
+++ app/src/main/java/com/test/mysamplepush/FireBaseMessagingService.kt
@@ -3,13 +3,21 @@ package com.test.mysamplepush
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Build
 import android.support.v4.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import android.graphics.BitmapFactory
+import java.net.URL
+
 
 class FireBaseMessagingService : FirebaseMessagingService() {
+
+    private var bigPicture: Bitmap? = null
 
     /**
      * 구글 토큰을 얻는 값입니다.
@@ -29,9 +37,23 @@ class FireBaseMessagingService : FirebaseMessagingService() {
 
     private fun sendNotification(remoteMessage: RemoteMessage?) {
 
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this,0,intent,
+            PendingIntent.FLAG_ONE_SHOT)
+
         val title = remoteMessage?.data?.get("title")
         val message = remoteMessage?.data?.get("message")
+        val myImgUrl = remoteMessage?.data?.get("image")
 
+
+        //이미지 온라인 링크를 가져와 비트맵으로 바꾼다.
+        try {
+            val url = URL(myImgUrl)
+            bigPicture = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
@@ -54,6 +76,14 @@ class FireBaseMessagingService : FirebaseMessagingService() {
                 .setChannelId(channel)
                 .setAutoCancel(true)
                 .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
+                .setStyle(NotificationCompat.BigTextStyle()
+                    .setBigContentTitle("FCM PUSH BIG TEXT")
+                    .bigText(message))
+                .setStyle(NotificationCompat.BigPictureStyle()
+                    .bigPicture(bigPicture)
+                    .setBigContentTitle("FCM Push")
+                    .setSummaryText(message))
+                .setContentIntent(pendingIntent)
 
             val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.notify(9999, notificationBuilder.build())
@@ -65,6 +95,15 @@ class FireBaseMessagingService : FirebaseMessagingService() {
                 .setContentTitle(message)
                 .setAutoCancel(true)
                 .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
+                .setStyle(NotificationCompat.BigTextStyle()
+                    .setBigContentTitle("FCM PUSH BIG TEXT")
+                    .bigText(message))
+                .setStyle(NotificationCompat.BigPictureStyle()
+                    .bigPicture(bigPicture)
+                    .setBigContentTitle("FCM Push")
+                    .setSummaryText(message))
+                .setContentIntent(pendingIntent)
+
 
             val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.notify(9999, notificationBuilder.build())
