0357edfc8a42501d8ce9938e373abb3b1413ba5f
eunyeoul <eunyull102@nate.com>
2019-05-08 17:47:25 +0900

    [update] 푸시 이미지 , 색상변경

--- app/src/main/java/com/test/mysamplepush/FireBaseMessagingService.kt
+++ app/src/main/java/com/test/mysamplepush/FireBaseMessagingService.kt
@@ -12,6 +12,9 @@ import android.support.v4.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.net.Uri
+import android.os.Bundle
 import java.net.URL
 
 
@@ -35,17 +38,40 @@ class FireBaseMessagingService : FirebaseMessagingService() {
 
     }
 
+    /**
+
+    ## FLAG_CANCEL_CURRENT : 이전에 생성한 PendingIntent는 취소하고 새로 만든다.
+
+    ## FLAG_IMMUTABLE : 생성된 PendingIntent 는 수정 불가능하도록 한다.
+
+    ## FLAG_NO_CREATE : 생성된 PendingIntent 를 반환한다.(재사용 가능)
+
+    ## FLAG_ONE_SHOT : 해당 Flag로 생성한 PendingIntent 는 일회성이다.
+
+    ## FLAG_UPDATE_CURRENT : 이미 생성된 PendingIntent가 존재하면 해당 Intent의 내용을 바꾼다.
+
+     */
     private fun sendNotification(remoteMessage: RemoteMessage?) {
-
-        val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(this,0,intent,
-            PendingIntent.FLAG_ONE_SHOT)
-
         val title = remoteMessage?.data?.get("title")
         val message = remoteMessage?.data?.get("message")
         val myImgUrl = remoteMessage?.data?.get("image")
+        val linkUrl = remoteMessage?.data?.get("linkurl")
 
+//        val intent = Intent(this, MainActivity::class.java)
+
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(linkUrl))
+//        browerIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+//
+//        val bundle = Bundle()
+//        bundle.putString("url", linkUrl)
+//
+//        intent.putExtras(bundle)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, browserIntent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
 
         //이미지 온라인 링크를 가져와 비트맵으로 바꾼다.
         try {
@@ -55,9 +81,9 @@ class FireBaseMessagingService : FirebaseMessagingService() {
             e.printStackTrace()
         }
 
+        val channel = "채널"
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
-            val channel = "채널"
             val channel_nm = "채널명"
 
             val notiChannel = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
@@ -72,36 +98,47 @@ class FireBaseMessagingService : FirebaseMessagingService() {
             val notificationBuilder = NotificationCompat.Builder(this, channel)
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setContentTitle(title)
+                .setLargeIcon(bigPicture)
                 .setContentText(message)
                 .setChannelId(channel)
                 .setAutoCancel(true)
                 .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
-                .setStyle(NotificationCompat.BigTextStyle()
-                    .setBigContentTitle("FCM PUSH BIG TEXT")
-                    .bigText(message))
-                .setStyle(NotificationCompat.BigPictureStyle()
-                    .bigPicture(bigPicture)
-                    .setBigContentTitle("FCM Push")
-                    .setSummaryText(message))
+                .setStyle(
+                    NotificationCompat.BigTextStyle()
+                        .setBigContentTitle("FCM PUSH BIG TEXT")
+                        .bigText(message)
+                )
+                .setStyle(
+                    NotificationCompat.BigPictureStyle()
+                        .bigPicture(bigPicture)
+                        .setBigContentTitle("FCM Push")
+                        .setSummaryText(message)
+                )
                 .setContentIntent(pendingIntent)
 
             val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.notify(9999, notificationBuilder.build())
 
         } else {
-            val notificationBuilder = NotificationCompat.Builder(this,"")
-                .setSmallIcon(R.drawable.ic_launcher_background)
+            val notificationBuilder = NotificationCompat.Builder(this, channel)
+                .setSmallIcon(R.drawable.ic_action_name)
                 .setContentTitle(title)
+                .setLargeIcon(bigPicture)
                 .setContentTitle(message)
-                .setAutoCancel(true)
+                .setAutoCancel(false)
                 .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
-                .setStyle(NotificationCompat.BigTextStyle()
-                    .setBigContentTitle("FCM PUSH BIG TEXT")
-                    .bigText(message))
-                .setStyle(NotificationCompat.BigPictureStyle()
-                    .bigPicture(bigPicture)
-                    .setBigContentTitle("FCM Push")
-                    .setSummaryText(message))
+                .setColor(Color.BLUE)
+                .setStyle(
+                    NotificationCompat.BigTextStyle()
+                        .setBigContentTitle("FCM PUSH BIG TEXT")
+                        .bigText(message)
+                )
+                .setStyle(
+                    NotificationCompat.BigPictureStyle()
+                        .bigPicture(bigPicture)
+                        .setBigContentTitle("FCM Push")
+                        .setSummaryText(message)
+                )
                 .setContentIntent(pendingIntent)
 
 
--- app/src/main/java/com/test/mysamplepush/MainActivity.kt
+++ app/src/main/java/com/test/mysamplepush/MainActivity.kt
@@ -2,18 +2,33 @@ package com.test.mysamplepush
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
 import android.util.Log
+import android.view.View
 import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
+
+    private var target_url: String? = "https://www.daum.net/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener { instanceIdResult ->
-            val newToken = instanceIdResult.token
-            Log.e("onCreate",newToken)
-        }
+        window.decorView.systemUiVisibility = 0
+
+//        if(!TextUtils.isEmpty("aaaa")){
+//            FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener { instanceIdResult ->
+//                val newToken = instanceIdResult.token
+//                Log.e("onCreate",newToken)
+//            }
+//        }
+
+        val intent = intent
+        val bunldle = intent.extras
+        target_url = bunldle?.getString("url")
+
+
+
     }
 }
